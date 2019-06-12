867c68070ab71f1aa7298f6ab09d34c7c76a32dd
Grace Tan <gracetan37@gmail.com>
2019-06-12 14:17:37 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -76,6 +76,7 @@ holiday_hash.each do |season,holiday|
   puts "#{season}: #{holiday}"
 
 end
+end
 
 def all_holidays_with_bbq(holiday_hash)
   # return an array of holiday names (as symbols) where supply lists
